3c2bbef69ddd7c16885b53863daa310c1d3448fe
Jason Gostowski <jasong@shipmentscans.griffininternal.com>
2016-09-06 11:07:24 -0500

    it works

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
@@ -8,67 +8,17 @@
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <link rel="stylesheet" type="text/css" href="main.css">
 </head>
-
 <body>
-<?php
-  include 'vars.php';
-  $PO = $_GET['PO'];  
-  
-  $db = new mysqli('localhost', $SQLUser, $SQLPass, 'packslipgen');
-  $result = $db->query('SELECT * FROM Orders WHERE `PO_Num` = '.$PO);
-  
-  $this_line = $result->fetch_assoc();
-?>
-
-<div class="container">
-  <div class="row">
-    <h2 class="col-sm-12 text-center">Shipment Detail List</h2>
+  <div class="container">
+    <div class="col-sm-3">
+      <b>Put that PO number righ'chere ---></b>
+    </div>
+    <div class="col-sm-9">
+      <form action="display.php">
+        <input type="text" name="PO"></input>
+        <input type="submit" value="Lay it on me"></input>
+      </form>
+    </div>
   </div>
-  <div class="row">
-    <div class="col-sm-6"><b>Customer: &nbsp</b><?php echo $this_line['Name']; ?></div>
-    <div class="col-sm-6 text-right"><b>PO: &nbsp</b><?php echo $this_line['PO_Num']; ?></div>
-  </div>
-  <div class="row">
-    <div class="col-sm-6"><b>Address: &nbsp</b><?php echo $this_line['Address']; ?></div>
-    <div class="col-sm-6 text-right"><b>Order# &nbsp</b><?php echo $this_line['SO_Num']; ?></div>
-  </div>
-  <div class="row">
-    <div class="col-sm-6"><b>Carrier: &nbsp</b><?php echo $this_line['Ship_Via']; ?></div>
-    <div class="col-sm-6 text-right"><b>Ship Date: &nbsp</b><?php echo $this_line['Ship_Date']; ?></div>
-  </div>
-
-  <div class="row">
-    <section class="col-sm-12">
-      <table>
-        <tr>
-          <th>Line</th><th>Part Number</th><th>Ship Quantity</th><th>Master Packs</th>
-        </tr>
-        <tr>
-          <?php
-            echo "<td>".$this_line['Line']."</td><td>".$this_line['SKU']."</td>";
-            echo "<td>".$this_line['Ship_Qty']."</td>";
-            echo "<td>".($this_line['Ship_Qty'] / $this_line['Casepack'])."</td>";
-          ?>
-        </tr>
-<?php
-  while (3 < 5) {
-    $this_line = $result->fetch_assoc();
-    if ($this_line == NULL) {
-      break;
-    }
-    else {
-      echo "<tr>";
-      echo "<td>".$this_line['Line']."</td><td>".$this_line['SKU']."</td>";
-      echo "<td>".$this_line['Ship_Qty']."</td>";
-      echo "<td>".($this_line['Ship_Qty'] / $this_line['Casepack'])."</td>";
-      echo "</tr>";
-      }
-  }
-?>
-      </table>
-    </section>
-  </div>
-</div>
-
 </body>
-</html> 
+</html>
